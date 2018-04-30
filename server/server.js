5042be1b09c71b85690c2b2aa676c3425e569eab
Philippe Loup <philippe.loup@gmail.com>
2018-04-30 08:48:41 -0400

    start server

---
+++ server/server.js
@@ -0,0 +1,18 @@
+require('./config');
+
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname,'../public');
+
+
+const port = process.env.PORT;
+
+var app = express();
+
+app.use(express.static(publicPath));
+
+
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
+});
