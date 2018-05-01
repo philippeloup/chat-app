07b27ff5b75163a4802dc7dd7a9b83c9c0cd789b
Philippe Loup <philippe.loup@gmail.com>
2018-05-01 08:58:48 -0400

    add new message and create message event

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+var socket = io();
+
+socket.on('connect', function ()  {
+  console.log('Connected to server');
+
+  socket.emit('createMessage', {
+    from: 'Andrew111',
+    text: 'What is up!'
+  })
+});
+
+socket.on('disconnect', function ()  {
+  console.log('Disconnected from server');
+});
+
+
+socket.on('newMessage', function (email) {
+  console.log('New email', email);
+})
--- server/server.js
+++ server/server.js
@@ -15,10 +15,22 @@ app.use(express.static(publicPath));
 
 io.on('connection',(socket) => {
   console.log('New user connected');
+
+  socket.emit('newMessage', {
+    from: 'mike2000',
+    text: 'Hey, What is going on?',
+    createdAt: 123
+  });
+
+  socket.on('createMessage', (newMessage) => {
+    console.log('createEmail', newMessage);
+  })
+
   socket.on('disconnect', () => {
     console.log('Client disconnected');
   })
 });
+
 
 server.listen(port, () => {
   console.log(`Started on port ${port}`);
