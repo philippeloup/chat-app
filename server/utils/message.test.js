bc3938bef0362cb793f4c4f9c695450204e3bb58
Philippe Loup <philippe.loup@gmail.com>
2018-05-02 08:27:20 -0400

    create generateMessage utility

---
+++ server/utils/message.test.js
@@ -0,0 +1,17 @@
+var expect = require('expect');
+
+const app = require('./../server');
+var {generateMessage} = require('./message');
+
+describe('generateMessage', () => {
+  it('should generate the correct message object', () => {
+    var from = 'Admin'
+    var text = 'Welcome to the chat app'
+    var message = generateMessage(from,text);
+    expect(message.from).toEqual(from);
+    expect(message.text).toEqual(text);
+    expect(typeof message.createdAt).toEqual('number')
+
+
+  });
+});
