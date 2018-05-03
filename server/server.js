bf2138c833608625769695cecab681e98698dfa1
Philippe Loup <philippe.loup@gmail.com>
2018-05-03 17:33:01 -0400

    add geolocatin url and test

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT;
 var app = express();
@@ -21,15 +21,18 @@ io.on('connection',(socket) => {
 
   socket.broadcast.emit('newMessage',generateMessage('Admin','A new user joined'));
 
-  socket.on('createMessage', (message) => {
+  socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
-
+    callback('This is from the server')
   });
 
+  socket.on('createLocationMessage', (coords) => {
+    io.emit('newLocationMessage', generateLocationMessage('Admin',coords.latitude, coords.longitude))
+  })
   socket.on('disconnect', () => {
     console.log('Client disconnected');
-  })
+  });
 });
 
 
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,7 +1,7 @@
 var expect = require('expect');
 
 const app = require('./../server');
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate the correct message object', () => {
@@ -11,7 +11,14 @@ describe('generateMessage', () => {
     expect(message.from).toEqual(from);
     expect(message.text).toEqual(text);
     expect(typeof message.createdAt).toEqual('number')
+  });
+});
 
-
+describe('generateLocationMessage',() => {
+  it('should generate correct location object', () => {
+    var from = 'Admin';
+    var longitude = 1;
+    var message = generateLocationMessage(from, longitude, longitude);
+    expect(message.url).toEqual('https:www.google.com/maps?q=1,1')
   });
 });
